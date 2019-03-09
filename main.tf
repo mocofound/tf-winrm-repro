57504587aee14fa53e07f24aa6e50df5c7e7973f
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-03-09 12:18:36 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,18 @@ resource "random_string" "password" {
 #  location = "eastus"
 #}
 
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "centralus"
+    resource_group_name = "${var.azurerm_resource_group}"
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
 # Create subnet
 resource "azurerm_subnet" "myterraformsubnet" {
     name                 = "mySubnet"
