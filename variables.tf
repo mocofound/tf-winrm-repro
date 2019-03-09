8a2214978bfaf5de3ac522831af19c0f0b8fddba
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-03-09 11:48:39 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable subnet_id {}
 
 # azurerm provider
-variable subscription_id {}
-variable client_id {}
-variable client_secret {}
-variable tenant_id {}
+#variable subscription_id {}
+#variable client_id {}
+#variable client_secret {}
+#variable tenant_id {}
