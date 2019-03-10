f6dd484df961fa19c5430368543a6e0ea3bb80fc
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-03-10 11:11:22 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -166,7 +166,7 @@ resource "null_resource" "cluster" {
 
 data "azurerm_public_ip" "test" {
   name                = "${azurerm_public_ip.test.name}"
-  resource_group_name = "${azurerm_virtual_machine.test.resource_group_name}"
+  resource_group_name = "${var.azurerm_resource_group}"
 }
 
 output "vm_ip_address" {
