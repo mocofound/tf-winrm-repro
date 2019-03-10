24cf16da4bbf66d870922b649ca85a115044f6e4
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-03-10 12:19:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,13 +95,13 @@ resource "azurerm_network_interface" "nic" {
   name = "mharen-test"
   location = "centralus"
   resource_group_name = "${var.azurerm_resource_group}"
+  network_security_group_id     = "${azurerm_network_security_group.myterraformnsg.id}"
 
   ip_configuration {
     name                          = "private_ip_address"
     subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
     private_ip_address_allocation = "dynamic"
     public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
-    network_security_group_id     = "${azurerm_network_security_group.myterraformnsg.id}"
   }
 }
 
