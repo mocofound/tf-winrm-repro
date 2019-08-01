5355bb3211a8f831f8da55f15d57018aee455d30
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-08-01 13:14:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
     location            = "centralus"
+    resource_group_name = "${azurerm_resource_group.group.name}"
 
 
     tags {
